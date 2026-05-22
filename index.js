7ef418ed7073265240f4cc17f9a9ed0f328af2a2
tofsir7 <khtofsir9191@gmail.com>
2026-05-22 17:13:40 +0600

    add homepage route

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
+const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
 
+console.log(CUSTOM_OBJECT_TYPE);
+const hubspotHeaders = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  'Content-Type': 'application/json',
+};
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+  try {
+    const properties = 'name,publisher,price';
 
+    const response = await axios.get(
+      `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`,
+      {
+        headers: hubspotHeaders,
+        params: {
+          properties,
+          limit: 100,
+        },
+      }
+    );
+
+    res.render('homepage', {
+      pageTitle: 'Custom Object Table | Integrating With HubSpot I Practicum',
+      records: response.data.results,
+    });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send('There was an error loading your custom object records.');
+  }
+});
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
@@ -24,8 +56,8 @@ const PRIVATE_APP_ACCESS = '';
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
+*/
+/*/App.get sample
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
@@ -41,7 +73,7 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
+/* * App.post sample
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
